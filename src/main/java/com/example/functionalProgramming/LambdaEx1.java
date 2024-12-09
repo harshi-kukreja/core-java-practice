9afe8d55f849cb1825b88710c67dec57e068751e
Sunil <sunilgudle7844@gmail.com>
2024-12-10 05:25:41 +0530

    Added the programs with Lambda and streams

---
+++ src/main/java/com/example/functionalProgramming/LambdaEx1.java
@@ -0,0 +1,18 @@
+package com.example.functionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaEx1 {
+    public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList(10, 45, 65, 90, 66, 99, 43, 944);
+
+        nums.forEach(num -> System.out.print(num + ", "));
+
+        System.out.println("filtered out the numbers");
+        nums.stream()
+                .filter(num -> num%5==0)
+                .forEach(num -> System.out.print(num+ ", "));
+    }
+}
