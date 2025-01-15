2d5d6b04197b6843016996acdfa01060f582c72a
Sunil <sunilgudle7844@gmail.com>
2025-01-15 10:49:56 +0530

    Added the programs asked in the recent interviews

---
+++ src/test/java/com/example/interviewQuestions/MergeTwoArray.java
@@ -0,0 +1,23 @@
+package com.example.interviewQuestions;
+
+import java.util.Arrays;
+
+// merge the given two arrays
+
+public class MergeTwoArray {
+    public static void main(String[] args) {
+        int[] num1 = {1,2,3};
+        int[] num2 = {2, 5, 6};
+
+        int[] merge = new int[num1.length + num1.length];
+
+        System.arraycopy(num1, 0, merge, 0, num2.length);
+        System.arraycopy(num2, 0, merge, num1.length, num2.length);
+
+        Arrays.sort(merge);
+
+        System.out.println(Arrays.toString(merge));
+
+
+    }
+}
